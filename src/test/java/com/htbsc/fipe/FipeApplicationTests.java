acfa63d3f88bd0e1260e6290ef59cc8e33f4ba10
Higor Tadeu <higorse@gmail.com>
2025-02-11 22:46:42 -0300

    Ajuste da versão do Java de 23 para 21

---
+++ src/test/java/com/htbsc/fipe/FipeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.htbsc.fipe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FipeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
