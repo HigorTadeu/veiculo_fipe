6285ec2945b07b9747b460483cba72890d7bf1ea
Higor Tadeu <higorse@gmail.com>
2025-02-11 22:49:17 -0300

    Adicionada a dependência Jackson para tratar JSON

--- src/main/java/com/htbsc/fipe/FipeApplication.java
+++ src/main/java/com/htbsc/fipe/FipeApplication.java
@@ -1,13 +1,18 @@
 package com.htbsc.fipe;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class FipeApplication {
+public class FipeApplication implements CommandLineRunner {
 
 	public static void main(String[] args) {
 		SpringApplication.run(FipeApplication.class, args);
 	}
 
+	@Override
+	public void run(String... args) throws Exception {
+		System.out.println("Primeira aplicação Java");
+	}
 }
